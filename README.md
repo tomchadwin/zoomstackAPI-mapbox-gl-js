b3cb7fb7db09531d5cea55593c613517795ff4dc
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2018-07-31 12:00:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# example_mapboxgl_js_without_key
+Showing a simple MapboxGL.js map without an API key
---
+++ main.js
@@ -0,0 +1,9 @@
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'style.json',
+    hash: true,
+    zoom: 11,
+    pitch:0,
+    bearing: 0,
+    center: [4.8, 52.4]
+});
